e8be552b18560d189e08142911bd63a5d5a33b0a
Dede <specialized8393@gmail.com>
2024-09-09 14:29:09 +0100

    Registered models agency to admin panel

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from agency.models import Trip, Category, Date, Day
 
-# Register your models here.
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ("title", "destination", "price", "max_members", "current_members")
+    search_fields = ("title", "destination")
+    list_filter = ("category",)
+    # additional configurations as necessary
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Date)
+class DateAdmin(admin.ModelAdmin):
+    list_display = ("trip", "from_date", "to_date")
+    list_filter = ("trip",)
+
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ("trip", "place", "activity")
+    list_filter = ("trip",)
