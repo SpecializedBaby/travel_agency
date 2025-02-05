834e221b4bc1c0739ac9da40f68cbe9c92de060f
Dede <specialized8393@gmail.com>
2025-02-05 20:19:35 +0000

    Implemented admin panel with Inline fields

--- agency/admin.py
+++ agency/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
-from agency.models import Trip, Category, Date, Day
+from django.utils.safestring import mark_safe
+
+from .models import (
+    Trip, TripPhoto, ProgramByDay, IncludedFeature,
+    TripDate, TripRequest, FAQ
+)
+
+
+class TripPhotoInline(admin.TabularInline):
+    model = TripPhoto
+    extra = 1
+    fields = ('photo', 'type', 'caption')
+    readonly_fields = ('photo_preview',)
+
+    def photo_preview(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 100px;" />')
+        return "Нет фото"
+    photo_preview.allow_tags = True
+    photo_preview.short_description = "Предпросмотр"
+
+
+class ProgramByDayInline(admin.TabularInline):
+    model = ProgramByDay
+    extra = 1
+    fields = ('day_number', 'title', 'description', 'accommodation', 'meal_plan')
+
+
+class IncludedFeatureInline(admin.TabularInline):
+    model = IncludedFeature
+    extra = 1
+    fields = ('title', 'description', 'icon')
+
+
+class TripDateInline(admin.TabularInline):
+    model = TripDate
+    extra = 1
+    fields = ('start_date', 'end_date', 'price', 'current_members', 'is_special_offer', 'icon')
+
+
+class FAQInline(admin.TabularInline):
+    model = FAQ
+    extra = 1
+    fields = ('question', 'answer', 'order')
 
 
 @admin.register(Trip)
