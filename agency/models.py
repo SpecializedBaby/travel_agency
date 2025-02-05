ad68ac22e863cbaf849f3ef7c3394edc8cb31156
Dede <specialized8393@gmail.com>
2025-02-05 15:53:17 +0000

    create available_spots property  for TripDate and add icon field

--- agency/models.py
+++ agency/models.py
@@ -87,47 +87,20 @@ class TripDate(models.Model):
     start_date = models.DateField("Дата начала")
     end_date = models.DateField("Дата окончания")
     price = models.DecimalField("Цена", max_digits=10, decimal_places=0)
-    available_spots = models.PositiveIntegerField("Доступные места")
+    current_members = models.PositiveIntegerField("Количество брони")
     is_special_offer = models.BooleanField("Спецпредложение", default=False)
+    icon = models.CharField("Иконка офера (FontAwesome)", max_length=30, blank=True)
 
     def clean(self):
         if self.start_date >= self.end_date:
             raise ValidationError("Дата окончания должна быть позже даты начала")
 
+    @property
+    def available_spots(self):
+        return self.trip.group_size - self.current_members
+
     def __str__(self):
         return f"{self.start_date} - {self.end_date} ({self.price}€)"
-
-
-class FAQ(models.Model):
-    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='faqs')
-    question = models.CharField("Вопрос", max_length=255)
-    answer = models.TextField("Ответ")
-    order = models.PositiveIntegerField("Порядок", default=0)
-
-    class Meta:
-        ordering = ['order']
-
-
-class TripPhoto(models.Model):
-    PHOTO_TYPE_CHOICES = [
-        ('main', 'Главное фото'),
-        ('gallery', 'Фото галереи'),
-        ('slide', 'Фото слайдера'),
-    ]
-
-    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='photos')
-    photo = models.ImageField(upload_to=day_image_upload_path)
-    type = models.CharField(max_length=7, choices=PHOTO_TYPE_CHOICES)
-    caption = models.CharField("Подпись", max_length=100, blank=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['trip', 'type'],
-                condition=models.Q(type='main'),
-                name='unique_main_photo'
-            )
-        ]
 
 
 class TripRequest(models.Model):
