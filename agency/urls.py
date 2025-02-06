43c32e7d75ee2c8ebf82a28c25ecc6b876bee932
ex <specialized8393@gmail.com>
2025-02-06 14:03:57 +0000

    implemented TripSerializer with all fields & add TripViewSet with this serializer and create router api/

--- agency/urls.py
+++ agency/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 
 from agency.views import index
 
+from .views import TripViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'trips', TripViewSet)
+
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("api/", include(router.urls)),
 ]
 
 
--- agency/views.py
+++ agency/views.py
@@ -1,5 +1,14 @@
+from rest_framework import viewsets, status
 from django.shortcuts import render
+
+from agency.models import Trip
+from agency.serializers import TripSerializer
 
 
 def index(request):
     return render(request, "agency/index.html")
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
