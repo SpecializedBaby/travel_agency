76706a64176c8ac511ac1e539afacc5df23774bb
ex <specialized8393@gmail.com>
2025-02-10 23:42:03 +0000

    Updated serializers.py

--- agency/serializers.py
+++ agency/serializers.py
@@ -1,4 +1,3 @@
-from django.conf.urls.static import static
 from rest_framework import serializers
 from .models import Review, Sociallink, FAQ, TripRequest, TripDate, IncludedFeature, ProgramByDay, TripPhoto, Trip
 
@@ -10,7 +9,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_at']
 
 
-class SociallinkSerializer(serializers.ModelSerializer):
+class SocialLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sociallink
         fields = ['id', 'name', 'icon', 'url', ]
@@ -31,14 +30,18 @@ class TripDateSerializer(serializers.ModelSerializer):
         model = TripDate
         fields = ['id', 'formatted_start_date', 'formatted_end_date', 'available_spots', 'price', 'current_members', ]
 
-    def get_available_spots(self, obj):
+    @staticmethod
+    def get_available_spots(obj):
         return obj.trip.group_size - obj.current_members
 
-    def get_formatted_start_date(self, obj):
+    @staticmethod
+    def get_formatted_start_date(obj):
         return obj.start_date.strftime('%d %b, %Y')
 
-    def get_formatted_end_date(self, obj):
+    @staticmethod
+    def get_formatted_end_date(obj):
         return obj.end_date.strftime('%d %b, %Y')
+
 
 class IncludedFeatureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -55,43 +58,76 @@ class ProgramByDaySerializer(serializers.ModelSerializer):
 class TripPhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TripPhoto
-        fields = ['id', 'photo', 'type', 'caption', ]
+        fields = ['photo', ]
 
 
-class TripReviewSerializer(serializers.ModelSerializer):
+class TripRetrieveSerializer(serializers.ModelSerializer):
     photos = TripPhotoSerializer(many=True, read_only=True)
     program_by_days = ProgramByDaySerializer(many=True, read_only=True)
     included_features = IncludedFeatureSerializer(many=True, read_only=True)
     trip_dates = TripDateSerializer(many=True, read_only=True)
+    faqs = FAQSerializer(many=True, read_only=True)
 
     class Meta:
         model = Trip
         fields = [
             'id', 'status', 'title', 'country', 'welcome_message', 'duration_days', 'accommodation',
             'bonus', 'ask_title', 'description', 'created_at', 'photos', 'program_by_days',
-            'included_features', 'trip_dates', 'group_size', 'leaders',
+            'included_features', 'trip_dates', 'group_size', 'leaders', 'faqs',
         ]
 
-class TripListSerializer(TripReviewSerializer):
+class TripListSerializer(serializers.ModelSerializer):
     photo = serializers.SerializerMethodField()
+    start_date = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField(read_only=True)
+    country = serializers.SerializerMethodField()
+    available_spots = serializers.SerializerMethodField()
 
     class Meta:
         model = Trip
-        fields = ['id', 'photo', 'status', 'title', 'country', 'duration_days', 'group_size', 'trip_dates']
+        fields = ['id', 'start_date', 'price', 'available_spots', 'photo', 'status', 'title', 'country', 'duration_days', 'group_size', ]
 
     @staticmethod
     def get_photo(obj):
-        slide_photos = obj.photos.filter(type='slide')
-        random_photo = slide_photos.order_by('?').first()
-        return TripPhotoSerializer(random_photo).data if random_photo else None
+        slide_photos = getattr(obj, 'slide_photos', [])
+        slide_photo = slide_photos[0] if slide_photos else None
+        return slide_photo.photo.url if slide_photo else None
+
+    @staticmethod
+    def get_price(obj):
+        trip_dates = getattr(obj, 'trip_dates_list', [])
+        price = trip_dates[0].price
+        return price if price else 0.00
+
+    @staticmethod
+    def get_start_date(obj):
+        trip_dates = getattr(obj, 'trip_dates_list', [])
+        return trip_dates[0].start_date.strftime('%d %b, %Y') if trip_dates else None
+
+    @staticmethod
+    def get_country(obj):
+        return obj.get_country_display()
+
+    @staticmethod
+    def get_available_spots(obj):
+        trip_dates = getattr(obj, 'trip_dates_list', [])
+        return trip_dates[0].available_spots if trip_dates else None
 
 
 class TripRequestSerializer(serializers.ModelSerializer):
-    trip = TripListSerializer(read_only=True)
+    trip = serializers.SlugRelatedField(queryset=Trip.objects.all(), slug_field='slug', )
+
     class Meta:
         model = TripRequest
-        fields = ['id', 'trip', 'name', 'phone', 'email', 'preferred_contact', 'notes', 'created_at', ]
+        fields = ['trip', 'name', 'phone', 'email', 'preferred_contact', 'notes', 'created_at', ]
         read_only_fields = ['created_at', ]
 
 
+class CountrySerializer(serializers.Serializer):
+    country = serializers.CharField()
+    country_name = serializers.SerializerMethodField()
+    photo = serializers.CharField()
 
+    @staticmethod
+    def get_country_name(obj):
+        return dict(Trip.COUNTRY_CHOICES).get(obj["country"], obj["country"])
